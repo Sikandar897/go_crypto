2a2f1e110d1b1a3acfb0fda84a296cf9f5131bcf
Sikandar897 <sikandarafridi6@gmail.com>
2024-07-04 01:55:15 +0500

    add: splash view, assets

--- lib/view/splash.dart
+++ lib/view/splash.dart
@@ -10,16 +10,69 @@ class SplashView extends StatefulWidget {
 class _SplashViewState extends State<SplashView> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // width: 200,
-      // height: 200,
-      child: const Center(
-          child: Text(
-        'my splash',
-        style: TextStyle(
-          color: Colors.deepOrange,
+    double mywidth = MediaQuery.of(context).size.width;
+    double myheight = MediaQuery.of(context).size.height;
+    return SafeArea(
+        child: Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        height: myheight,
+        width: mywidth,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Image.asset('assets/image/1.gif'),
+            const Column(
+              children: [
+                Text(
+                  'The Future',
+                  style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'Learn more about cryptocurrency, look to',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.grey),
+                ),
+                Text(
+                  ' the future in IO Crypto',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.grey),
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: mywidth * 0.14),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: const Color(0xfffBC700),
+                    borderRadius: BorderRadius.circular(50)),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: mywidth * 0.05, vertical: myheight * 0.013),
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Create Portfolio',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.normal),
+                      ),
+                      RotationTransition(
+                        turns: AlwaysStoppedAnimation(310 / 360),
+                        child: Icon(Icons.arrow_forward_rounded),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
-      )),
-    );
+      ),
+    ));
   }
 }
