5f5a9ef7a07d430c4d2029c169c07289ad3a1f1e
Sikandar897 <sikandarafridi6@gmail.com>
2024-07-07 14:57:11 +0500

    add: decoration on homepage

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -10,6 +10,64 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    double myWidth = MediaQuery.of(context).size.width;
+    double myHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Container(
+        height: myHeight,
+        width: myWidth,
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+              Color.fromARGB(255, 255, 219, 73),
+              Color(0xffFBC700)
+            ])),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  //  horizontal: myWidth * 0.05,
+                  vertical: myHeight * 0.03),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: myWidth * 0.02, vertical: myHeight * 0.005),
+                    decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.5),
+                        borderRadius: BorderRadius.circular(5)),
+                    child: const Text(
+                      'Main Portfolio',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                  const Text(
+                    'Top 10 coins',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  const Text(
+                    'Experimental',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: myHeight,
+              width: myWidth,
+              decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(50),
+                      topRight: Radius.circular(50)),
+                  color: Colors.white),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
